69a54781337ba30d08e2f77c112cf244b0a98059
Rajkumarjittaaboyini <rjittaab@cisco.com>
2025-09-03 19:29:37 +0530

    see

---
+++ github/src/test/java/Github11/Gitpull.java
@@ -0,0 +1,24 @@
+package Github11;
+		
+
+	import org.junit.jupiter.api.Test;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import io.github.bonigarcia.wdm.WebDriverManager;
+
+	import static org.junit.jupiter.api.Assertions.assertEquals;
+
+	public class Gitpull{
+	    @Test
+	    public void openGoogleTest() {
+	        WebDriverManager.chromedriver().setup();
+	        WebDriver driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	        driver.get("https://www.google.com");
+
+	        // Verify page title
+	        assertEquals("Google", driver.getTitle());
+
+	        driver.quit();
+	    }
+	}
